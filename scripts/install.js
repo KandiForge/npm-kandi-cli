57d2f6d6a5e9c25b1f4583292137da9db02a5a18
KentAls <kent@abstractclass.net>
2025-08-10 01:31:47 -0700

    Update to kandi-cli v4.2.2
    - Updated install script to download v4.2.2
    - Matches current KandiForge app version

--- scripts/install.js
+++ scripts/install.js
@@ -6,7 +6,7 @@ const https = require('https');
 const { execSync } = require('child_process');
 const crypto = require('crypto');
 
-const RELEASE_VERSION = '1.0.2';
+const RELEASE_VERSION = '4.2.2';
 const REPO = 'KandiForge/kandi-cli-releases';
 
 function getArch() {
